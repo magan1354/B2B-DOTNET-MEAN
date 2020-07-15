4c06d2b3961763e3d3b3a0c660433f103ab34ed6
Maganjot Singh <maganjotsingh@gmail.com>
2020-07-15 16:36:43 +0530

    question-2 stub

---
+++ Question-2 - Stub/WebApiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApiApplicationExample
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+	    config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+        }
+    }
+}
